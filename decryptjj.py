287e0467695500a7caa7b3444a4c1da407d21b5a
jarekj9 <jerry.esp.sha@gmail.com>
2019-09-16 18:58:20 +0200

    v1.3

--- decryptjj.py
+++ decryptjj.py
@@ -1,7 +1,12 @@
-#ver1.2
+#ver1.3
 #reads encrypted files from folder encrypteddata, decrypts and writes output to folder decrypteddata
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 import os
+import base64
+import bz2
 
 def give_filenames(name):
 	filenames=[]
@@ -20,18 +25,44 @@ except:
 	exit(0)
 
 
-fkey = Fernet(b'citj91dDrAgWQRusrotOHPb4HHE8uvT-vd8XCD43r2Q=')
+def ferenet_from_password(password):
+	password = bytes(password.encode('utf-8'))
+	salt = b'31415926538'
+	kdf = PBKDF2HMAC(
+		algorithm=hashes.SHA256(),
+		length=32,
+		salt=salt,
+		iterations=100000,
+		backend=default_backend()
+		)
+	key = base64.urlsafe_b64encode(kdf.derive(password))
+	return(Fernet(key))
 
-print("Version 1.2, Jaroslaw Jankun")
+
+#default key:
+fkey = Fernet(b'citj91dDrAgWQRusrotOHPb4HHE8uvT-vd8XCD43r2Q=')
+#default key is replaced if another key is found in file 'secret'
+for file in os.listdir():
+	if file == 'secret':
+		print('Using secret file...')
+		with open ('secret','r') as file:
+			password= file.readline().strip()
+			fkey = ferenet_from_password(password)
+
+print("Version 1.3, Jaroslaw Jankun")
 print('There are {number} files to decrypt:'.format( number= str(len(filenames))) )
 
 for filename in filenames: 
 	print('Decrypting '+filename)
 	
 	with open ('encrypteddata/'+filename,'rb') as file:
-		encrypteddata=file.read()
+		encrypteddata = file.read()
+	
+	compresseddata = fkey.decrypt(encrypteddata)
+	cleardata = bz2.decompress(compresseddata)
+	
 	
 	with open ('cleardata/'+filename[:-6],'wb') as decryptedfile:   #[:-6] to delete extension: .encjj
-		decryptedfile.write(fkey.decrypt(encrypteddata))
+		decryptedfile.write(cleardata)
 		
 	os.remove('encrypteddata/'+filename)
--- encryptjj.py
+++ encryptjj.py
@@ -1,8 +1,13 @@
-#ver1.2
+#ver1.3
 #encrypts all files in folder cleardata and writes output files to encrypteddata folder
 
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 import os
+import base64
+import bz2
 
 def give_filenames(name):
 	filenames=[]
@@ -21,9 +26,37 @@ except:
 	exit(0)
 
 
-fkey = Fernet(b'citj91dDrAgWQRusrotOHPb4HHE8uvT-vd8XCD43r2Q=')
+def ferenet_from_password(password):
+	password = bytes(password.encode('utf-8'))
+	salt = b'31415926538'
+	kdf = PBKDF2HMAC(
+		algorithm=hashes.SHA256(),
+		length=32,
+		salt=salt,
+		iterations=100000,
+		backend=default_backend()
+		)
+	key = base64.urlsafe_b64encode(kdf.derive(password))
+	return(Fernet(key))
 
-print("Version 1.2, Jaroslaw Jankun")
+
+
+
+
+#default key:
+fkey = Fernet(b'citj91dDrAgWQRusrotOHPb4HHE8uvT-vd8XCD43r2Q=')
+#default key is replaced if another key is found in file 'secret'
+for file in os.listdir():
+	if file == 'secret':
+		print('Using secret file...')
+		with open ('secret','r') as file:
+			password= file.readline().strip()
+			fkey = ferenet_from_password(password)
+
+
+
+
+print("Version 1.3, Jaroslaw Jankun")
 print('There are {number} files to encrypt:'.format( number= str(len(filenames))) )
 
 for filename in filenames: 
@@ -31,8 +64,11 @@ for filename in filenames:
 	
 	with open ('cleardata/'+filename,'rb') as file:
 		cleardata=file.read()
-
+		
+	compresseddata = bz2.compress(cleardata)
+	encrypteddata = fkey.encrypt(compresseddata)
+	
 	with open ('encrypteddata/'+filename+'.encjj','wb') as encryptedfile:
-		encryptedfile.write(fkey.encrypt(cleardata))
+		encryptedfile.write(encrypteddata)
 		
 	os.remove('cleardata/'+filename)
